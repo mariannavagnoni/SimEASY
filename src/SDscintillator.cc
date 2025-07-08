c315149ed52e5929b8d3c776f3b0dead1e6fc3ae
Thomas Chillery <thomas.chillery@lngs.infn.it>
2025-07-08 12:52:34 +0200

    [Minor optimise] Comment unused things in ProcessHits - important to keep code light and optimise, especially for this Geant function

--- src/SDscintillator.cc
+++ src/SDscintillator.cc
@@ -22,48 +22,39 @@ MySensitiveDetector::~MySensitiveDetector()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void MySensitiveDetector::Initialize(G4HCofThisEvent* hce)
-{
-    
+{ 
     //if (collectionID<0) collectionID=GetCollectionID(0);
-        G4int collectionID = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
+    G4int collectionID = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
     
-    G4cout << "collectionID" << collectionID << std::endl;
-    
-    
-    
-        hitsCollection = new MyHitsCollection(SensitiveDetectorName, collectionName[0]);
-        
-        
-        
-        hce->AddHitsCollection(collectionID, hitsCollection);
-    
+    //G4cout << "collectionID" << collectionID << std::endl;
+
+    hitsCollection = new MyHitsCollection(SensitiveDetectorName, collectionName[0]);
+            
+    hce->AddHitsCollection(collectionID, hitsCollection);
 }
 
 G4bool MySensitiveDetector::ProcessHits(G4Step *step, G4TouchableHistory *ROhist)
 {
-    
-   
     //Accessing the current step
 
     G4StepPoint* preStepPoint = step->GetPreStepPoint();
-    G4StepPoint* postStepPoint = step ->GetPostStepPoint();
-    G4ThreeVector pos1 = preStepPoint->GetPosition();
-    G4ThreeVector pos2 = postStepPoint->GetPosition();
+    //G4StepPoint* postStepPoint = step->GetPostStepPoint();
+    //G4ThreeVector pos1 = preStepPoint->GetPosition();
+    //G4ThreeVector pos2 = postStepPoint->GetPosition();
 
     //G4cout << "initial position: " << pos1 << G4endl;
     //G4cout << "final position: " << pos2 << G4endl;
     //G4cout << "ooOOOooo" << std::endl;
     
     G4TouchableHandle theTouchable = preStepPoint->GetTouchableHandle();
-    G4int copyNo = theTouchable->GetCopyNumber();
+    //G4int copyNo = theTouchable->GetCopyNumber();
     //G4cout << "Copy number of the scint volume: " << copyNo << G4endl;
     //G4int motherCopyNo = theTouchable->GetVolume(1)->GetCopyNo();
     //G4cout << "Copy number of the vacuum volume: " << motherCopyNo << G4endl;
     G4int grandMotherCopyNo = theTouchable->GetCopyNumber(2);
     //G4cout << "Copy number of the mother volume: " << grandMotherCopyNo << G4endl;
    
-    
-    G4double stepLength = step ->GetStepLength();
+    //G4double stepLength = step ->GetStepLength();
     //G4cout << "Step Length: " << stepLength /CLHEP::mm << G4endl;
     
     MyHit* hit = new MyHit();
@@ -77,14 +68,8 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *step, G4TouchableHistory *ROhist
     
 
     return true;
-
-         
-    
-    
-    
 }
 
 void MySensitiveDetector::EndOfEvent(G4HCofThisEvent* hce)
 {
-    
- }
+}
